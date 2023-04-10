e4f86687bfc273c3c2b074e439a692533dde158e
Vivek Acharya <98654927+vivekacharyarules@users.noreply.github.com>
2023-04-10 22:45:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("chat-form");
+const input = document.getElementById("chat-input");
+const messages = document.getElementById("chat-messages");
+const apiKey = "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";
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const message = input.value;
+  input.value = "";
+
+  messages.innerHTML += `<div class="message user-message">
+  <img src="user.png" alt="user icon"> <span>${message}</span>
+  </div>`;
+
+  // Use axios library to make a POST request to the OpenAI API
+  const response = await axios.post(
+    "https://api.openai.com/v1/completions",
+    {
+      prompt: message,
+      model: "text-davinci-003",
+      temperature: 0.5,
+      max_tokens: 800,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+    }
+  );
+  const chatbotResponse = response.data.choices[0].text;
+
+  messages.innerHTML += `<div class="message bot-message">
+  <img src="chatbot.png" alt="bot icon"> <span>${chatbotResponse}</span>
+  </div>`;
+});
