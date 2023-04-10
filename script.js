3e5bd7da5c743c7bdacbb0a01a4ede691b149738
Vivek Acharya <98654927+vivekacharyarules@users.noreply.github.com>
2023-04-10 23:37:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("chat-form");
 const input = document.getElementById("chat-input");
 const messages = document.getElementById("chat-messages");
-const apiKey = "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";
+const apiKey = "";
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
